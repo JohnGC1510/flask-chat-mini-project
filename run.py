b927f83ec5a10577f9d3d84c36c8671b374d1921
JohnGC1510 <johngc1510@gmail.com>
2020-09-14 11:24:49 +0000

    Added a chatbox for messages and refactored code

--- run.py
+++ run.py
@@ -1,35 +1,47 @@
 import os
-from flask import Flask, redirect
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session, url_for
 
 app = Flask(__name__)
+app.secret_key = "string"
 messages = []
 
 
-def add_messages(username, message):
-    messages.append("{}: {}".format(username, message))
+def add_message(username, message):
+    """strptime() gives the format, %H = hours in 24 hour clock etc."""
+    now = datetime.now().strftime("%H:%M:%S")
+    # now converts date-time object to string
+    messages.append({"timestamp": now, "from": username, "message": message})
 
 
-def get_all_messages():
-    return "<br>".join(messages)
-
-
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
     """Main page with instructions"""
-    return "To send a message use /USERNAME/MESSAGE"
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+        # if form is posted create new variable in session called username
+    if "username" in session:
+        # if username exists then direct to username view
+        return redirect(url_for("user", username=session["username"]))
+
+    return render_template("index.html")
 
 
-@app.route("/<username>")  # username declared as variable
+@app.route("/chat/<username>", methods=["GET", "POST"])
+# username declared as variable
 def user(username):
-    """Display chat messages"""
-    return "<h1>Welcome, {0}</h1> {1}".format(username, get_all_messages())
-
-
-@app.route("/<username>/<message>")
-def send_message(username, message):
-    """Create a new message and redirect to chat page"""
-    add_messages(username, message)
-    return redirect(username)  # redirect back to username view
+    """Add and display chat messages"""
+    if(request.method) == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_message(username, message)
+        return redirect(session["username"])
+        """ Without the retrun redirect every time
+        the page refreshes it resends the POST data amd will
+        keep re-sending the same message, after redirect POST
+        will be false and chat.html will render"""
+    return render_template(
+        "chat.html", username=username, chat_messages=messages)
 
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
